80b879bc4629a4fa75fde7abc1580b75b8743448
James Liu <liuxjames88@gmail.com>
2024-04-15 15:46:18 -0400

    getting new york counties visualization working

--- xinfo/src/App.jsx
+++ xinfo/src/App.jsx
@@ -1,25 +1,50 @@
-import { useState, useEffect } from 'react';
-import { MapContainer, TileLayer, GeoJSON } from 'react-leaflet';
+import { useState } from 'react';
+import DeckGL from '@deck.gl/react';
+import { GeoJsonLayer } from '@deck.gl/layers';
+import geojsonData from './assets/new-york-counties.json';
 
 function App() {
-  const center = [40.7128, -74.0060]; // Center of the map (New York in this case)
-  const [geojsonData, setGeojsonData] = useState(null);
+  const [hoverInfo, setHoverInfo] = useState(null);
 
-  useEffect(() => {
-    // Fetch GeoJSON data
-    fetch('./assets/new-york-counties.geojson')
-      .then((response) => response.json())
-      .then((data) => setGeojsonData(data));
-  }, []);
+  const layer = new GeoJsonLayer({
+    id: 'GeoJsonLayer',
+    data: geojsonData,
+    stroked: false,
+    filled: true,
+    pickable: true,
+    getFillColor: (f) => {
+      // Use the feature's properties to generate a color
+      const hash = [...f.properties.name].reduce((acc, char) => char.charCodeAt(0) + ((acc << 5) - acc), 0);
+      return [hash & 0xFF, (hash & 0xFF00) >> 8, (hash & 0xFF0000) >> 16];
+    },
+    getLineColor: [255, 255, 255],
+    lineWidthMinPixels: 1,
+    onHover: (info) => setHoverInfo(info),
+  });
+
+  const INITIAL_VIEW_STATE = {
+    longitude: -74.5,
+    latitude: 40.7,
+    zoom: 9,
+    pitch: 0,
+    bearing: 0
+  };
 
   return (
-    <MapContainer center={center} zoom={13} style={{ height: "100vh", width: "100%" }}>
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    <div>
+      <DeckGL
+        initialViewState={INITIAL_VIEW_STATE}
+        controller={true}
+        layers={[layer]}
       />
-      {geojsonData && <GeoJSON data={geojsonData} />}
-    </MapContainer>
+      {hoverInfo && hoverInfo.object && (
+        <div style={{position: 'absolute', zIndex: 1, pointerEvents: 'none', left: hoverInfo.x, top: hoverInfo.y}}>
+          <div style={{background: 'white', padding: '4px', border: '1px solid #ccc'}}>
+            {hoverInfo.object.properties.name}
+          </div>
+        </div>
+      )}
+    </div>
   );
 }
 
